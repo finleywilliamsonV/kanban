66b45e6b472d4c31917c48133cf438cf1d7a7871
Daniil Veremchuk <daniel@verydanny.com>
2018-11-15 13:04:41 -0500

    new lint rules

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import { render } from 'react-dom'
 
 import { Hello } from './components/Hello'
 
-const someObject = { bool: true }
-
 render(
   <Hello compiler='TypeScript' framework='React' />,
   document.getElementById('example'),
